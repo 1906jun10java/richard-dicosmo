6127c436c002cae675db2d391e71e170bb82e37a
Richard Di Cosmo <rjdicosmo@gmail.com>
2019-06-14 15:44:56 -0400

    new file:   RDiCosmo-PSpenceCalculatorChallange/Calculator.java 	new file:   RDiCosmo-PSpenceCalculatorChallange/MainUserControl.java 	new file:   RDiCosmo-PSpenceCalculatorChallange/Reader.java

---
+++ RDiCosmo-PSpenceCalculatorChallange/Calculator.java
@@ -0,0 +1,42 @@
+package com.revature.calculator;
+
+public class Calculator {
+
+	public static double subtract(double ...a) {
+		double answer = a[0];
+		
+		for(int i =1; i< a.length; i++) {
+			answer = answer - a[i];
+		}
+		
+		
+
+
+		return answer;
+	}
+
+	public static double add(double ...a) {
+		double answer = 0;
+
+
+		return answer;
+	}
+
+	public static double multiply(double ...a) {
+		double answer = 0;
+
+
+		return answer;
+
+
+	}
+	
+	public static double divide(double ...a) {
+		double answer = 0;
+
+
+		return answer;
+		
+	}
+
+}
